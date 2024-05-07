09ac71ca45f1732ffde19292e438c1faa5117f84
eliavammi <141130886+eliavammi@users.noreply.github.com>
2024-05-07 11:00:19 +0200

    Update views.py

--- myproject/myapi/views.py
+++ myproject/myapi/views.py
@@ -3,26 +3,20 @@ from django.http import HttpResponse, JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from rest_framework.parsers import JSONParser
 from myapi.models import React
-from myapi.serializers import ReactSerializers
+from myapi.serializers import ReactSerializers, UserSerializer
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from rest_framework import status
+from rest_framework import status, generics, permissions
+from rest_framework.views import APIView
+from rest_framework.authtoken.models import Token
+from django.contrib.auth import authenticate
+from django.contrib.auth.models import User
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import AllowAny
+from django.contrib.auth import authenticate, login
 
-
-@api_view(['GET'])
-def hello_world(request):
-    return Response({'message': 'Only Hello!'})
-
-@api_view(['GET'])
-def hello_all(request):
-    return Response({'hello': 'Hello All!'})
-
-@api_view(['POST'])
-def hello_react(request):
-    return Response({'react': request.data})
-    
 @csrf_exempt
-def react_list(request):
+def react_list(request, format=None):
     if request.method == 'GET':
         react = React.objects.all()
         serializer = ReactSerializers(react, many=True)
@@ -37,7 +31,7 @@ def react_list(request):
         return JsonResponse(serializer.errors, status=400)
 
 @api_view(['GET', 'PUT', 'DELETE'])
-def react_detail(request, pk):
+def react_detail(request, pk, format=None):
     try:
         react = React.objects.get(pk=pk)
     except React.DoesNotExist:
@@ -47,6 +41,46 @@ def react_detail(request, pk):
         serializer = ReactSerializers(react)
         return Response(serializer.data)
 
+    elif request.method == 'PUT':
+        serializer = ReactSerializers(react, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.error, status=status.HTTP_404_NOT_FOUND) 
+
     elif request.method == 'DELETE':
         react.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
+
+@csrf_exempt
+def user_register(request):
+    if request.method == 'GET':
+        user = User.objects.all()
+        serializer = UserSerializer(user, many=True)
+        return JsonResponse(serializer.data, safe=False)
+
+    
+    elif request.method == 'POST':
+        data = JSONParser().parse(request)
+        serializer = UserSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data, status=201)
+        return JsonResponse(serializer.errors, status=400)
+
+class UserDetailAPI(APIView):
+  authentication_classes = (TokenAuthentication,)
+  permission_classes = (AllowAny,)
+  def get(self,request,*args,**kwargs):
+    user = User.objects.get(id=request.user.id)
+    serializer = UserSerializer(user)
+    return Response(serializer.data)
+
+class UserLoginView(APIView):
+    def post(self, request):
+        user = authenticate(username=request.data['username'], password=request.data['password'])
+        if user:
+            token, created = Token.objects.get_or_create(user=user)
+            return Response({'token': token.key})
+        else:
+            return Response({'error': 'Invalid credentials'}, status=401)
