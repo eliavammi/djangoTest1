dcccb2cd10318a9a37f35d8a9879c1009f72a035
eliavammi <141130886+eliavammi@users.noreply.github.com>
2024-05-07 10:59:40 +0200

    Update serializers.py

--- myproject/myapi/serializers.py
+++ myproject/myapi/serializers.py
@@ -1,24 +1,35 @@
 from rest_framework import serializers
 from myapi.models import React
+from django.contrib.auth.models import User
+from rest_framework.authtoken.models import Token
+from django.dispatch import receiver
+from django.conf import settings
+from django.db.models.signals import post_save
 
-# class BookSerializer(serializers.ModelSerializer):
-#     class Meta:
-#         model = Book
-#         fields = (
-#             "id",
-#             "title",
-#             "author",
-#             "description",
-#             "sales",
-#             "published",
-#             "created_at",
-#             "updated_at",
-#         )
+
 
 class ReactSerializers(serializers.ModelSerializer):
     class Meta:
         model = React
-        fields = ['id', 'name']
+        fields = ['id', 'name', 'cognome']
 
         def create(self, validate_data):
             return React.objects.create(**validate_data)
+
+class UserSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+
+    def create(self, validate_data):
+            return User.objects.create_user(
+                username=validate_data['username'],
+                password=validate_data['password'],
+            )
+
+    class Meta: 
+        model = User
+        fields = ['username', 'email', 'password']
+
+    @receiver(post_save, sender=settings.AUTH_USER_MODEL)
+    def create_auth_token(sender, instance=None, created=False, **kwargs):
+        if created:
+            Token.objects.create(user=instance)
