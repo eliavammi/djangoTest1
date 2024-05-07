2fbc46e902053e3dcfca4a2b2f303f1cd0f249b3
eliavammi <141130886+eliavammi@users.noreply.github.com>
2024-05-07 10:59:55 +0200

    Update urls.py

--- myproject/myapi/urls.py
+++ myproject/myapi/urls.py
@@ -1,14 +1,18 @@
 from django.urls import path
 from . import views
+from rest_framework.urlpatterns import format_suffix_patterns
+from .views import UserDetailAPI, UserLoginView
 
 # router = routers.DefaultRouter()
 # router.register(r'books', views.BookList, basename='books')
 
 urlpatterns = [
     # path('', include(router.urls)),
-    path('hello-world/', views.hello_world, name='hello_world'),
-    path('hello-all/', views.hello_all, name='hello_all'),
-    path('hello-react/', views.hello_react, name='hello_react'),
     path('react/', views.react_list),
     path('react_detail/<int:pk>/', views.react_detail),
+    path('user/', views.user_register),
+    path("get-details/", UserDetailAPI.as_view()),
+    path('user-login/', UserLoginView.as_view())
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
