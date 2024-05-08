8a2ef4256f569f93f835e8639b7382f4ba6b5023
eliavammi <141130886+eliavammi@users.noreply.github.com>
2024-05-08 11:33:50 +0200

    Update ComponentRequest.js, select item + creation date and last update

--- ComponentRequest.js
+++ ComponentRequest.js
@@ -2,21 +2,15 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 function ComponentRequest() {
-  const [message, setMessage] = useState('');
   const [inputValue, setInputValue] = useState('')
   const [cognome, setCognome] = useState('')
   const [items, setItems] = useState([])
+  const [nameFilter, setNameFilter] = useState('')
+  const [select, setSelect] = useState('')
+  const [selected, setSelected] = useState([])
 
   useEffect(() => {
-    axios.get('http://localhost:9735/api/hello-world/')
-      .then(response => {
-        setMessage(response.data.message);
-      })
-      .catch(error => {
-        console.log(error);
-      });
-
-      axios.get('http://localhost:9735/api/react/')
+    axios.get('http://localhost:9735/api/react/')
       .then(response =>{
         setItems(response.data)
       })
@@ -27,7 +21,7 @@ function ComponentRequest() {
 
   return (
     <div>
-      <p>{message}</p>
+      {/* <p>{message}</p> */}
       <br/>
         <input 
             type='text'
@@ -35,7 +29,7 @@ function ComponentRequest() {
             onChange={(event) => {
                 setInputValue(event.currentTarget.value)
             }}
-            placeholder='name'
+            placeholder='nome'
         />
         <input 
             type='text'
@@ -61,20 +55,66 @@ function ComponentRequest() {
         <div>
           <ul>
         {items.map(i=>
-        <div>
+        <div key={i.id}>
           <p 
             key={i.id}
-          onClick={() => {
-            axios.delete(`http://localhost:9735/api/react_detail/${i.id}`).then(response => {
+          >
+            {`${i.id}. ${i.name} / ${i.cognome} / creation date: ${new Date(i.creationDate).toLocaleString()} / last update: ${new Date(i.updateDate).toLocaleString()}` }<button
+            onClick={() => {
+              axios.put(`http://localhost:9735/api/react_detail/${i.id}/`, {name: inputValue === '' ? i.name : inputValue, cognome: cognome === '' ? i.cognome : cognome})
+              .then(response => axios.get(`http://localhost:9735/api/react/`).then(response => setItems(response.data)).catch(error => console.log(error)))
+              .catch(error => console.log(error))
+              setCognome('')
+              setInputValue('')
+            }}
+          >
+            UPDATE
+          </button>
+          <button
+            onClick={() => {
+              axios.delete(`http://localhost:9735/api/react_detail/${i.id}`).then(response => {
               axios.get('http://localhost:9735/api/react/').then(response => setItems(response.data)).catch(error => console.log(error))
             }).catch(error => console.log(error))
-          }}
+            }}
           >
-            {`${i.id}. ${i.name} ${i.cognome}` }
+            ELIMINA
+          </button>
           </p>
         </div>
-      )}
-      </ul>
+        )}
+          </ul>
+        </div>
+        <div>
+          {/* <lable>Name Filter</lable><br/>
+          <input type='text' name='nameFilter' value={nameFilter} onChange={(event) => setNameFilter(event.currentTarget.value)} placeholder='Name Filter'/>
+          <button
+            onClick={()=>{
+              axios.post('http://localhost:9735/api/react-namefil/', {name:nameFilter}).then(response => console.log(response)).catch(error => console.log(error))
+            }}
+          >
+            SEND
+          </button> */}
+          <br/><br/>
+          <select value={select} onChange={(event)=>{setSelect(event.currentTarget.value)}}>
+            {items.map(i => (
+              <option value={i.id} key={i.id}>{`${i.name} ${i.cognome}`}</option>
+            ))}
+          </select>
+          <button
+            onClick={()=>{
+              axios.get(`http://localhost:9735/api/react_detail/${select}/`,{})
+              .then(response => {
+                setSelected([response.data])
+                console.log(response)
+              })
+              .catch(error => console.log(error))
+            }}
+          >
+            SEND
+          </button>
+            <br/>
+            {/* {selected.map(s => (<p>`${s.name} ${s.cognome}`)</p>)} */}
+            {selected.map(s => <p>{`${s.name} ${s.cognome}`}</p>)}
         </div>
     </div>
   );
