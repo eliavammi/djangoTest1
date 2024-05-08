36123e22f8ca662867508e992dee82f432e60879
eliavammi <141130886+eliavammi@users.noreply.github.com>
2024-05-08 11:34:33 +0200

    Update models.py, creationDate, updateDate

--- myproject/myapi/models.py
+++ myproject/myapi/models.py
@@ -1,5 +1,13 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.utils import timezone
+from datetime import datetime
+
+
 
 class React(models.Model):
     name = models.CharField(max_length=100)
+    cognome = models.CharField(max_length=100, default='nessuno')
+    creationDate = models.DateTimeField(auto_now_add=True, blank=True)
+    updateDate = models.DateTimeField(auto_now=True, blank=True)
+    
